d085731dc3dd74aac85da4c56c2b773afa455356
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-05-18 14:44:58 +0200

    fix home.js NOW WORKING

--- js/home.js
+++ js/home.js
@@ -1,138 +1,73 @@
-const homeImages640 = { // images width: 640px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_640.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_640.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_640.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_640.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_640.jpg'
+const homeImages = {
+	640: [
+		'/img/homepage/from_my_west_03_WEB_640.jpg',
+		'/img/homepage/i_have_never_17_WEB_640.jpg',
+		'/img/homepage/lying_18_WEB_640.jpg',
+		'/img/homepage/please_turn_out_02_WEB_640.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_640.jpg'
+	],
+	768: [
+		'/img/homepage/from_my_west_03_WEB_768.jpg',
+		'/img/homepage/i_have_never_17_WEB_768.jpg',
+		'/img/homepage/lying_18_WEB_768.jpg',
+		'/img/homepage/please_turn_out_02_WEB_768.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_768.jpg'
+	],
+	1068: [
+		'/img/homepage/from_my_west_03_WEB_1068.jpg',
+		'/img/homepage/i_have_never_17_WEB_1068.jpg',
+		'/img/homepage/lying_18_WEB_1068.jpg',
+		'/img/homepage/please_turn_out_02_WEB_1068.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_1068.jpg'
+	],
+	1472: [
+		'/img/homepage/from_my_west_03_WEB_1472r.jpg',
+		'/img/homepage/i_have_never_17_WEB_1472r.jpg',
+		'/img/homepage/lying_18_WEB_1472r.jpg',
+		'/img/homepage/please_turn_out_02_WEB_1472r.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
+	],
+	2136: [
+		'/img/homepage/from_my_west_03_WEB_2136r.jpg',
+		'/img/homepage/i_have_never_17_WEB_2136r.jpg',
+		'/img/homepage/lying_18_WEB_2136r.jpg',
+		'/img/homepage/please_turn_out_02_WEB_2136r.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
+	],
+	2880: [
+		'/img/homepage/from_my_west_03_WEB_2880rr.jpg',
+		'/img/homepage/i_have_never_17_WEB_2880rr.jpg',
+		'/img/homepage/lying_18_WEB_2880rr.jpg',
+		'/img/homepage/please_turn_out_02_WEB_2880rr.jpg',
+		'/img/homepage/right_and_wrong_06_WEB_2880rr.jpg'
+	]
+};
+	
+const getAvailableImageSizes = imagesObject => Object.keys(imagesObject);
+
+const getBestSizeToUse = (sizes, screenWidth) => {
+
+	for (const size of sizes) {
+		if (screenWidth < size) return size
 	}
+
+	return sizes[sizes.length - 1]
 };
 
-const homeImages768 = { // images width: 768px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_768.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_768.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_768.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_768.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_768.jpg'
-	}
+const pickARamdomImage = images => images[Math.floor(Math.random() * images.length)];
+
+const getRandomBestResImage = images => {
+	const availableSizes = getAvailableImageSizes(images)
+	const screenWidth = window.innerWidth
+	const bestSize = getBestSizeToUse(availableSizes, screenWidth)
+	
+	return pickARamdomImage(images[bestSize])
 };
 
-const homeImages1068 = { // images width: 1068px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_1068.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_1068.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_1068.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_1068.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_1068.jpg'
-	}
-};
+const selectedImage = getRandomBestResImage(homeImages);
 
-const homeImages1472r = { // images width: 1472px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_1472r.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_1472r.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_1472r.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_1472r.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
-	}
-};
-
-const homeImages2136r = { // images width: 2136px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_2136r.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_2136r.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_2136r.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_2136r.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
-	}
-};
-
-const homeImages2880r = { // images width: 2880px
-	1: {
-		imageUrl: '/img/homepage/from_my_west_03_WEB_2880r.jpg'
-	},
-	2: {
-		imageUrl: '/img/homepage/i_have_never_17_WEB_2880r.jpg'
-	},
-	3: {
-		imageUrl: '/img/homepage/lying_18_WEB_2880r.jpg'
-	},
-	4: {
-		imageUrl: '/img/homepage/please_turn_out_02_WEB_2880r.jpg'
-	},
-	5: {
-		imageUrl: '/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
-	}
-};
-
-
-function changeImage() {
-	let randomNumber = Math.floor((Math.random() * 5) + 1);
-	document.getElementById('background').style.background = 'url(\'' + setWidth[randomNumber].imageUrl + '\')';
+function applyImage(selectedImage){
+	document.getElementById('background').style.background = `url(${selectedImage})`;
 }
 
-let setWidth = function () {  
-	let correctWidth;
-	if (window.innerWidth < 641) {
-		correctWidth = homeImages640;
-	}
-	else if (window.innerWidth < 769) {
-		correctWidth = homeImages768;
-	}
-	else if (window.innerWidth < 1069) {
-		correctWidth = homeImages1068;
-	}
-	else if (window.innerWidth < 1473) {
-		correctWidth = homeImages1472r;
-	}
-	else if (window.innerWidth < 2137) {
-		correctWidth = homeImages2136r;
-	}
-	else {
-		correctWidth = homeImages2880r;
-	}
-
-};
-
-changeImage();
+applyImage(selectedImage);
