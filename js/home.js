f4cc28ee17c28b607c4962ea3fd6d9cb672a488f
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-05-18 16:53:05 +0200

    change random image, fix background cover with js

--- js/home.js
+++ js/home.js
@@ -2,42 +2,42 @@ const homeImages = {
 	640: [
 		'/img/homepage/from_my_west_03_WEB_640.jpg',
 		'/img/homepage/i_have_never_17_WEB_640.jpg',
-		'/img/homepage/lying_18_WEB_640.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_640.jpg',
 		'/img/homepage/please_turn_out_02_WEB_640.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_640.jpg'
 	],
 	768: [
 		'/img/homepage/from_my_west_03_WEB_768.jpg',
 		'/img/homepage/i_have_never_17_WEB_768.jpg',
-		'/img/homepage/lying_18_WEB_768.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_768.jpg',
 		'/img/homepage/please_turn_out_02_WEB_768.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_768.jpg'
 	],
 	1068: [
 		'/img/homepage/from_my_west_03_WEB_1068.jpg',
 		'/img/homepage/i_have_never_17_WEB_1068.jpg',
-		'/img/homepage/lying_18_WEB_1068.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_1068.jpg',
 		'/img/homepage/please_turn_out_02_WEB_1068.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_1068.jpg'
 	],
 	1472: [
 		'/img/homepage/from_my_west_03_WEB_1472r.jpg',
 		'/img/homepage/i_have_never_17_WEB_1472r.jpg',
-		'/img/homepage/lying_18_WEB_1472r.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
 		'/img/homepage/please_turn_out_02_WEB_1472r.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
 	],
 	2136: [
 		'/img/homepage/from_my_west_03_WEB_2136r.jpg',
 		'/img/homepage/i_have_never_17_WEB_2136r.jpg',
-		'/img/homepage/lying_18_WEB_2136r.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
 		'/img/homepage/please_turn_out_02_WEB_2136r.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
 	],
 	2880: [
 		'/img/homepage/from_my_west_03_WEB_2880rr.jpg',
 		'/img/homepage/i_have_never_17_WEB_2880rr.jpg',
-		'/img/homepage/lying_18_WEB_2880rr.jpg',
+		'/img/homepage/i_cant_relax_02_WEB_2880rr.jpg',
 		'/img/homepage/please_turn_out_02_WEB_2880rr.jpg',
 		'/img/homepage/right_and_wrong_06_WEB_2880rr.jpg'
 	]
@@ -48,26 +48,28 @@ const getAvailableImageSizes = imagesObject => Object.keys(imagesObject);
 const getBestSizeToUse = (sizes, screenWidth) => {
 
 	for (const size of sizes) {
-		if (screenWidth < size) return size
+		if (screenWidth < size) return size;
 	}
 
-	return sizes[sizes.length - 1]
+	return sizes[sizes.length - 1];
 };
 
 const pickARamdomImage = images => images[Math.floor(Math.random() * images.length)];
 
 const getRandomBestResImage = images => {
 	const availableSizes = getAvailableImageSizes(images)
-	const screenWidth = window.innerWidth
-	const bestSize = getBestSizeToUse(availableSizes, screenWidth)
+	const screenWidth = window.innerWidth;
+	const bestSize = getBestSizeToUse(availableSizes, screenWidth);
 	
-	return pickARamdomImage(images[bestSize])
+	return pickARamdomImage(images[bestSize]);
 };
 
 const selectedImage = getRandomBestResImage(homeImages);
+const imageContainer = document.getElementById('background');
 
 function applyImage(selectedImage){
-	document.getElementById('background').style.background = `url(.${selectedImage}) no-repeat center center fixed`;
+	// imageContainer.style.background = `url(.${selectedImage}) no-repeat center center fixed`;
+	imageContainer.style.cssText = `background: url(.${selectedImage}) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; overflow: hidden;`;
 }
 
 applyImage(selectedImage);
