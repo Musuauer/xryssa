db11de3310363a626d3d390fd6a79010da018773
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 15:54:52 +0100

    fix images path

--- src/Utils/home.js
+++ src/Utils/home.js
@@ -1,32 +1,32 @@
 export function coverImages () {
   const homeImages = {
     640: [
-      '/img/homepage/from_my_west_03_WEB_1068.jpg',
-      '/img/homepage/i_have_never_17_WEB_1068.jpg',
-      '/img/homepage/i_cant_relax_02_WEB_1068.jpg',
-      '/img/homepage/please_turn_out_02_WEB_1068.jpg',
-      '/img/homepage/right_and_wrong_06_WEB_1068.jpg'
+      '../../static/img/homepage/from_my_west_03_WEB_1068.jpg',
+      '../../static/img/homepage/i_have_never_17_WEB_1068.jpg',
+      '../../static/img/homepage/i_cant_relax_02_WEB_1068.jpg',
+      '../../static/img/homepage/please_turn_out_02_WEB_1068.jpg',
+      '../../static/img/homepage/right_and_wrong_06_WEB_1068.jpg'
     ],
     768: [
-      '/img/homepage/from_my_west_03_WEB_1472r.jpg',
-      '/img/homepage/i_have_never_17_WEB_1472r.jpg',
-      '/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
-      '/img/homepage/please_turn_out_02_WEB_1472r.jpg',
-      '/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
+      '../../static/img/homepage/from_my_west_03_WEB_1472r.jpg',
+      '../../static/img/homepage/i_have_never_17_WEB_1472r.jpg',
+      '../../static/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
+      '../../static/img/homepage/please_turn_out_02_WEB_1472r.jpg',
+      '../../static/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
     ],
     1068: [
-      '/img/homepage/from_my_west_03_WEB_2136r.jpg',
-      '/img/homepage/i_have_never_17_WEB_2136r.jpg',
-      '/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
-      '/img/homepage/please_turn_out_02_WEB_2136r.jpg',
-      '/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
+      '../../static/img/homepage/from_my_west_03_WEB_2136r.jpg',
+      '../../static/img/homepage/i_have_never_17_WEB_2136r.jpg',
+      '../../static/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
+      '../../static/img/homepage/please_turn_out_02_WEB_2136r.jpg',
+      '../../static/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
     ],
     1472: [
-      '/img/homepage/from_my_west_03_WEB_2880r.jpg',
-      '/img/homepage/i_have_never_17_WEB_2880r.jpg',
-      '/img/homepage/i_cant_relax_02_WEB_2880r.jpg',
-      '/img/homepage/please_turn_out_02_WEB_2880r.jpg',
-      '/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
+      '../../static/img/homepage/from_my_west_03_WEB_2880r.jpg',
+      '../../static/img/homepage/i_have_never_17_WEB_2880r.jpg',
+      '../../static/img/homepage/i_cant_relax_02_WEB_2880r.jpg',
+      '../../static/img/homepage/please_turn_out_02_WEB_2880r.jpg',
+      '../../static/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
     ]
   }
   console.log('coverimages working')
