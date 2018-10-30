d624e759c3c4e958b7ffe1bd264f42b80b879c75
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 14:43:12 +0100

    fix typo

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ProjectTemplate } from '../../templates/project'
-import '../../pages/stle.css'
+import '../../pages/style.css'
 
 const ProjectPagePreview = ({ entry, widgetFor }) => (
   <ProjectTemplate
