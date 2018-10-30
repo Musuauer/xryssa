ebd4119c965bdaafd1a5ca2264e29f28eb3a535c
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 16:50:37 +0100

    image test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,104 +7,107 @@ import * as thumbnails from '../Utils/thumbnails'
 
 export default class IndexPage extends React.Component {
   componentDidMount () {
-    this.coverImages()
+    const imageContainer = document.querySelector('html')
+
+    function applyImage () {
+      console.log('image container')
+      imageContainer.style.cssText = `background: url(../../../../static/img/by_reinventing_03_WEB_2880r.jpg) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; overflow: hidden;`
+    }
+
+    applyImage()
+    // this.coverImages()
     thumbnails.addScrollListener()
   }
 
-  coverImages = () => {
-    const homeImages = {
-      640: [
-        '../../static/img/homepage/from_my_west_03_WEB_1068.jpg',
-        '../../static/img/homepage/i_have_never_17_WEB_1068.jpg',
-        '../../static/img/homepage/i_cant_relax_02_WEB_1068.jpg',
-        '../../static/img/homepage/please_turn_out_02_WEB_1068.jpg',
-        '../../static/img/homepage/right_and_wrong_06_WEB_1068.jpg'
-      ],
-      768: [
-        '../../static/img/homepage/from_my_west_03_WEB_1472r.jpg',
-        '../../static/img/homepage/i_have_never_17_WEB_1472r.jpg',
-        '../../static/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
-        '../../static/img/homepage/please_turn_out_02_WEB_1472r.jpg',
-        '../../static/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
-      ],
-      1068: [
-        '../../static/img/homepage/from_my_west_03_WEB_2136r.jpg',
-        '../../static/img/homepage/i_have_never_17_WEB_2136r.jpg',
-        '../../static/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
-        '../../static/img/homepage/please_turn_out_02_WEB_2136r.jpg',
-        '../../static/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
-      ],
-      1472: [
-        '../../static/img/homepage/from_my_west_03_WEB_2880r.jpg',
-        '../../static/img/homepage/i_have_never_17_WEB_2880r.jpg',
-        '../../static/img/homepage/i_cant_relax_02_WEB_2880r.jpg',
-        '../../static/img/homepage/please_turn_out_02_WEB_2880r.jpg',
-        '../../static/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
-      ]
-    }
-    console.log('home images', homeImages)
+  // coverImages = () => {
+  //   const homeImages = {
+  //     640: [
+  //       '../static/img/homepage/from_my_west_03_WEB_1068.jpg',
+  //       '../static/img/homepage/i_have_never_17_WEB_1068.jpg',
+  //       '../static/img/homepage/i_cant_relax_02_WEB_1068.jpg',
+  //       '../static/img/homepage/please_turn_out_02_WEB_1068.jpg',
+  //       '../static/img/homepage/right_and_wrong_06_WEB_1068.jpg'
+  //     ],
+  //     768: [
+  //       '../static/img/homepage/from_my_west_03_WEB_1472r.jpg',
+  //       '../static/img/homepage/i_have_never_17_WEB_1472r.jpg',
+  //       '../static/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
+  //       '../static/img/homepage/please_turn_out_02_WEB_1472r.jpg',
+  //       '../static/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
+  //     ],
+  //     1068: [
+  //       '../static/img/homepage/from_my_west_03_WEB_2136r.jpg',
+  //       '../static/img/homepage/i_have_never_17_WEB_2136r.jpg',
+  //       '../static/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
+  //       '../static/img/homepage/please_turn_out_02_WEB_2136r.jpg',
+  //       '../static/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
+  //     ],
+  //     1472: [
+  //       '../static/img/homepage/from_my_west_03_WEB_2880r.jpg',
+  //       '../static/img/homepage/i_have_never_17_WEB_2880r.jpg',
+  //       '../static/img/homepage/i_cant_relax_02_WEB_2880r.jpg',
+  //       '../static/img/homepage/please_turn_out_02_WEB_2880r.jpg',
+  //       '../static/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
+  //     ]
+  //   }
+  //   console.log('home images', homeImages)
 
-    const getAvailableImageSizes = imagesObject => Object.keys(imagesObject)
+  //   const getAvailableImageSizes = imagesObject => Object.keys(imagesObject)
 
-    const getBestSizeToUse = (sizes, screenWidth) => {
-      for (const size of sizes) {
-        if (screenWidth < size) return size
-      }
+  //   const getBestSizeToUse = (sizes, screenWidth) => {
+  //     for (const size of sizes) {
+  //       if (screenWidth < size) return size
+  //     }
 
-      return sizes[sizes.length - 1]
-    }
+  //     return sizes[sizes.length - 1]
+  //   }
 
-    const pickARamdomImage = images => images[Math.floor(Math.random() * images.length)]
+  //   const pickARamdomImage = images => images[Math.floor(Math.random() * images.length)]
 
-    const getRandomBestResImage = images => {
-      console.log('uno')
-      const availableSizes = getAvailableImageSizes(images)
-      const screenWidth = window.innerWidth
-      const bestSize = getBestSizeToUse(availableSizes, screenWidth)
+  //   const getRandomBestResImage = images => {
+  //     console.log('uno')
+  //     const availableSizes = getAvailableImageSizes(images)
+  //     const screenWidth = window.innerWidth
+  //     const bestSize = getBestSizeToUse(availableSizes, screenWidth)
 
-      return pickARamdomImage(images[bestSize])
-    }
+  //     return pickARamdomImage(images[bestSize])
+  //   }
 
-    const selectedImage = getRandomBestResImage(homeImages)
-    const imageContainer = document.querySelector('html')
+  //   const selectedImage = getRandomBestResImage(homeImages)
+  //   const imageContainer = document.querySelector('html')
 
-    function applyImage (selectedImage) {
-      console.log('selected image', selectedImage)
-      console.log('image container', imageContainer)
-      imageContainer.style.cssText = `background: url(${selectedImage}) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; overflow: hidden;`
-    }
+  //   function applyImage (selectedImage) {
+  //     console.log('selected image', selectedImage)
+  //     console.log('image container', imageContainer)
+  //     imageContainer.style.cssText = `background: url(${selectedImage}) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; overflow: hidden;`
+  //   }
 
-    return applyImage(selectedImage)
-  }
+  //   return applyImage(selectedImage)
+  // }
 
   render () {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
-    var divStyle = {
-      background: 'url(../../static/img/homepage/i_cant_relax_02_WEB_1068.jpg)'
-    }
 
     return (
       <Layout>
-        <div style={divStyle}>
-          <div className='project-list'>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className='list-item'
-                  key={post.id}
-                >
+        <div className='project-list'>
+          {posts
+            .map(({ node: post }) => (
+              <div
+                className='list-item'
+                key={post.id}
+              >
 
-                  <Link className='link' to={post.frontmatter.path} onMouseOver={thumbnails.toggleThumbVisibility} onMouseOut={thumbnails.toggleThumbVisibility}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <Link to={post.frontmatter.path}>
-                    <img src={post.frontmatter.thumbnail} className='project-thumbnail' alt='thumbnail' />
-                  </Link>
+                <Link className='link' to={post.frontmatter.path} onMouseOver={thumbnails.toggleThumbVisibility} onMouseOut={thumbnails.toggleThumbVisibility}>
+                  {post.frontmatter.title}
+                </Link>
+                <Link to={post.frontmatter.path}>
+                  <img src={post.frontmatter.thumbnail} className='project-thumbnail' alt='thumbnail' />
+                </Link>
 
-                </div>
-              ))}
-          </div>
+              </div>
+            ))}
         </div>
 
       </Layout>
