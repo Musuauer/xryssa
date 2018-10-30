fcdc5cd71c3cd4660375a91a74999ba522803dfa
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 22:45:06 +0100

    fix cover images, add styled component

---
+++ src/Utils/coverImages.js
@@ -0,0 +1,7 @@
+export const coverImages = [
+  '/img/from_my_west_03_WEB_2880r.jpg',
+  '/img/i_have_never_17_WEB_2880r.jpg',
+  '/img/i_cant_relax_02_WEB_2880r.jpg',
+  '/img/please_turn_out_02_WEB_2880r.jpg',
+  '/img/right_and_wrong_06_WEB_2880r.jpg'
+]
