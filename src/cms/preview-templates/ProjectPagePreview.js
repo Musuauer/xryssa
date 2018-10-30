243725ef90d3a2eb128cc62501d2b7656596df3c
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 14:31:30 +0100

    import css into preview, test

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ProjectTemplate } from '../../templates/project'
+import '../pages/style.css'
 
 const ProjectPagePreview = ({ entry, widgetFor }) => (
   <ProjectTemplate
