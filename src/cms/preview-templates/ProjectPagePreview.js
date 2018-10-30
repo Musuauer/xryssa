9bfd5e77f49d42896f05ac0d032415051e9561db
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 14:41:25 +0100

    fix path

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ProjectTemplate } from '../../templates/project'
-import '../pages/style.css'
+import '../../pages/stle.css'
 
 const ProjectPagePreview = ({ entry, widgetFor }) => (
   <ProjectTemplate
