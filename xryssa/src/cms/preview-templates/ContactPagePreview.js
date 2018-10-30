39811463e5ece30b3fd7e07a392e62827ffd7952
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 12:05:04 +0100

    fix yml identation and preview imports

--- xryssa/src/cms/preview-templates/ContactPagePreview.js
+++ xryssa/src/cms/preview-templates/ContactPagePreview.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Contact } from '../../templates/contact-page'
-imp
+import Contact from '../../templates/contact-page'
 
 const ContactPagePreview = ({ entry, widgetFor }) => (
   <Contact
