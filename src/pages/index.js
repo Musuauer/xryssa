a1b6eb5f55592522769cddaff1a8d5d82680d0df
Musuauergit config --global user.email musuauer@gmail.com <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-10-30 16:10:17 +0100

    js test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,78 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import * as thumbnails from '../Utils/thumbnails'
-import { coverImages } from '../Utils/home'
+// import { coverImages } from '../Utils/home'
 
 export default class IndexPage extends React.Component {
   componentDidMount () {
-    coverImages()
+    this.coverImages()
     thumbnails.addScrollListener()
+  }
+
+  coverImages = () => {
+    const homeImages = {
+      640: [
+        '../../static/img/homepage/from_my_west_03_WEB_1068.jpg',
+        '../../static/img/homepage/i_have_never_17_WEB_1068.jpg',
+        '../../static/img/homepage/i_cant_relax_02_WEB_1068.jpg',
+        '../../static/img/homepage/please_turn_out_02_WEB_1068.jpg',
+        '../../static/img/homepage/right_and_wrong_06_WEB_1068.jpg'
+      ],
+      768: [
+        '../../static/img/homepage/from_my_west_03_WEB_1472r.jpg',
+        '../../static/img/homepage/i_have_never_17_WEB_1472r.jpg',
+        '../../static/img/homepage/i_cant_relax_02_WEB_1472r.jpg',
+        '../../static/img/homepage/please_turn_out_02_WEB_1472r.jpg',
+        '../../static/img/homepage/right_and_wrong_06_WEB_1472r.jpg'
+      ],
+      1068: [
+        '../../static/img/homepage/from_my_west_03_WEB_2136r.jpg',
+        '../../static/img/homepage/i_have_never_17_WEB_2136r.jpg',
+        '../../static/img/homepage/i_cant_relax_02_WEB_2136r.jpg',
+        '../../static/img/homepage/please_turn_out_02_WEB_2136r.jpg',
+        '../../static/img/homepage/right_and_wrong_06_WEB_2136r.jpg'
+      ],
+      1472: [
+        '../../static/img/homepage/from_my_west_03_WEB_2880r.jpg',
+        '../../static/img/homepage/i_have_never_17_WEB_2880r.jpg',
+        '../../static/img/homepage/i_cant_relax_02_WEB_2880r.jpg',
+        '../../static/img/homepage/please_turn_out_02_WEB_2880r.jpg',
+        '../../static/img/homepage/right_and_wrong_06_WEB_2880r.jpg'
+      ]
+    }
+    console.log('home images', homeImages)
+
+    const getAvailableImageSizes = imagesObject => Object.keys(imagesObject)
+
+    const getBestSizeToUse = (sizes, screenWidth) => {
+      for (const size of sizes) {
+        if (screenWidth < size) return size
+      }
+
+      return sizes[sizes.length - 1]
+    }
+
+    const pickARamdomImage = images => images[Math.floor(Math.random() * images.length)]
+
+    const getRandomBestResImage = images => {
+      console.log('uno')
+      const availableSizes = getAvailableImageSizes(images)
+      const screenWidth = window.innerWidth
+      const bestSize = getBestSizeToUse(availableSizes, screenWidth)
+
+      return pickARamdomImage(images[bestSize])
+    }
+
+    const selectedImage = getRandomBestResImage(homeImages)
+    const imageContainer = document.getElementById('background')
+
+    function applyImage (selectedImage) {
+      console.log('dos')
+
+      imageContainer.style.cssText = `background: url(.${selectedImage}) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; overflow: hidden;`
+    }
+
+    return applyImage(selectedImage)
   }
 
   render () {
